107e8925e25042cfcdf9028f4d4b2f2b8c2507c3
charlie-lyc <ofetwo@gmail.com>
2023-04-11 13:51:26 +0900

    fix home, about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,38 +1,52 @@
 import '../css/about.css'
-
+import longCOVID from "../assets/longCOVID.jpg"
+import ReactPlayer from "react-player"
 
 const About = () => {
 
     return (
         <div className='about'>
-            {/* 
-                <div className='subTitle'>Purpose of This Website</div>
-                <div className='aboutContent'>
-                    <p> Would creating a community for post-COVID teenagers allow them to feel supported and included,
-                        and give the potential for future long-COVID research?
-                    </p>
-                    <p>
-                        This website is created to provide a long COVID community for teenagers. After the acute infection of SARS-CoV-2 in 2022,
-                        I started to experience long COVID symptoms. Even though I visited doctors, I couldn't get effective treatment,
-                        and I felt I was the only student who was having the aftereffects. However, after reading long COVID research articles,
-                        I found out that the long COVID symptoms appear quite frequently for teenagers and adolescents.
-                        To provide an online community where teenagers can share their experiences and communicate to make them feel supported and included,
-                        and collect the data in easier way for researchers, I decided to create this website.
-                    </p>
-                </div> 
-            */}
-
             <div className='subTitle'>What is Long COVID</div>
             <div className='aboutContent'>
-            {/* <img src={이미지이름.jpeg} /> */}
-            <p>
-                Long-term symptoms experienced after the SARS-CoV-2 infection.
-                The symptoms are not only cardio respiratory related, but also are related to neuropsychiatric, gastrointestinal, and so on.
-                According to WHO, shortness of breath,brain fog, fatigue, and cough are most commonly experienced.
-                Due to its variety, it is not still clear to define whether it is post COVID-19 condition, and
-                further research is needed for clarification.
-            </p>
+
+                {/* Update 23.04.11 */}
+                <img className="imageSize" src={longCOVID} alt="long COVID" />
+                <div className="source">
+                    Christopher D. Vélez, MD. “Can Long Covid Affect the Gut?”
+                    Harvard Health, 20 Mar. 2023,
+                    https://www.health.harvard.edu/blog/can-long-covid-affect-the-gut-202303202903.
+                </div>
+                
+                <p>
+                    Long-term symptoms experienced after the SARS-CoV-2 infection.
+                    The symptoms are not only cardiorespiratory related, but also are
+                    related to neuropsychiatric, gastrointestinal, and so on.
+                    According to WHO, shortness of breath, brain fog, fatigue, and
+                    cough are most commonly experienced. Due to its variety, it is
+                    not still clear to define whether it is post COVID-19 condition,
+                    and further research is needed for clarification. Further
+                    information can be found at{" "}
+                    <a className="article" href="https://ncv.kdca.go.kr/hcp/page.do?mid=0102" target="_blank" rel="noreferrer">
+                        <b>KDCA</b>.
+                    </a>
+                </p>
+                
+                <br />
+                <div className="subTitle">Related Videos</div>
+                <div className="videos">
+                    <ReactPlayer url="https://www.youtube.com/watch?v=fF8xdaSzSMg" />
+                    <ReactPlayer url="https://www.youtube.com/watch?v=7ZF6XS2cpiw" />
+                    <br />
+                </div>
+                <div className="videos">
+                    <ReactPlayer url="https://www.youtube.com/watch?v=0w5XoN7GH-o" />
+                    <br />
+                    <ReactPlayer url="https://www.youtube.com/watch?v=5YDMTfesQWM" />
+                </div>
+                {/* Update 23.04.11 */}
+
             </div>
+
         </div>
     )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,35 @@
 import '../css/home.css'
+import FDACovid from "../assets/FDACovid.png"
+import DV1 from "../assets/DV1.png"
+import DV2 from "../assets/DV2.png"
+import DV3 from "../assets/DV3.png"
+import DV4 from "../assets/DV4.png"
+import DV5 from "../assets/DV5.png"
+import DV6 from "../assets/DV6.png"
+import DV7 from "../assets/DV7.png"
+import DV8 from "../assets/DV8.png"
 
 
 const Home = ({ username }) => {
     
     return (
         <div className='home'>
+
             <div className='subTitle'>Welcome, {username}!</div>
+            
             <div className='homeContent'>
-                <div className="subTitle">Purpose of This Website</div>
                 <br />
+                <div className="subTitle">Purpose of This Website</div>
+                
+                {/* Update 23.04.11 */}
+                <img className="imageSize" src={FDACovid} alt="FDA COVID" />
+                <div className="source">
+                    “COVID-19 Test Basics.” U.S. Food and Drug Administration, FDA,
+                    1 Apr. 2023,
+                    https://www.fda.gov/consumers/consumer-updates/kobideu-19-covid-19-geomsa-gibon-sahang.{" "}
+                </div>
+                {/* Update 23.04.11 */}
+
                 <div className="question">
                     {" "}
                     <b>
@@ -31,6 +52,25 @@ const Home = ({ username }) => {
                     supported and included, and collect the data in easier way for
                     researchers, I decided to create this website.
                 </p>
+
+                {/* Update 23.04.11 */}
+                <br />
+                <br />
+                <div className="subTitle">
+                {" "}
+                Data Visualizations Created by Using the Survey Sample of the
+                Website
+                </div>
+                <img className="dataVisImg" src={DV1} alt="DV1" />
+                <img className="dataVisImg" src={DV3} alt="DV3" />
+                <img className="dataVisImg" src={DV4} alt="DV4" />
+                <img className="dataVisImg" src={DV2} alt="DV2" />
+                <img className="dataVisImg" src={DV5} alt="DV5" />
+                <img className="dataVisImg" src={DV6} alt="DV6" />
+                <img className="dataVisImg" src={DV7} alt="DV7" />
+                <img className="dataVisImg" src={DV8} alt="DV8" />
+                {/* Update 23.04.11 */}
+
             </div>
         </div>
     )
